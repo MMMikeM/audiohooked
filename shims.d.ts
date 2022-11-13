7c1a947510cf7bcec1d0b85845066092f11520fb
MMMike <mikem.tba@gmail.com>
2022-11-13 15:49:47 +0200

    feat: :label: Improve types to allow btn prop on buttons

--- shims.d.ts
+++ shims.d.ts
@@ -4,11 +4,12 @@ declare module "solid-js" {
   namespace JSX {
     interface CustomHTMLAttributes {
       center?: boolean
+    }
+    interface CustomButtomAttrbutes {
       btn?: string
     }
-    interface HTMLAttributes<T>
-      extends AttributifyAttributes,
-        CustomHTMLAttributes {}
+    interface HTMLAttributes<T> extends AttributifyAttributes, CustomHTMLAttributes {}
+    interface ButtonHTMLAttributes<T> extends CustomButtomAttrbutes {}
     interface IntrinsicElements {
       row: HTMLAttributes<HTMLDivElement>
       column: HTMLAttributes<HTMLDivElement>
