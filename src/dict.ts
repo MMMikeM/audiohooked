27c6774b1fa01924c3f1fbf17dd1b952e4a9f701
MMMike <mikem.tba@gmail.com>
2022-11-13 14:41:20 +0200

    feat: :globe_with_meridians: Created translation strings

---
+++ src/dict.ts
@@ -0,0 +1,50 @@
+const dict = {
+  en: {
+    title: "Audiohooked",
+    points: [
+      ["An audio player"],
+      ["Internationalisation", "Only English, opensource translations"],
+      ["Stream or download files", "Maybe rely on google drive as a datastore"],
+      [
+        "Upload files",
+        "Maybe rely on google drive as a datastore",
+        "If data can't be found, user manually inputs",
+      ],
+      [
+        "Track and sync progress of the audiobook",
+        "This probably has to be in a db",
+        "Consideration to offline listening and syncing latest progress",
+      ],
+      [
+        "Display information about audiobook",
+        "Possibly a tool to scrape this?",
+      ],
+      ["Library", "Drag and drop categoriess"],
+      ["Controls"],
+      ["Timestamped notes"],
+    ],
+    features: "What features do I need?",
+    test: {
+      nest: {
+        testingNesting: "yay",
+      },
+    },
+  },
+  test: {
+    title: "TRANSLATED",
+    features: "TRANSLATED",
+    points: [
+      ["TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+      ["TRANSLATED", "TRANSLATED", "TRANSLATED", "TRANSLATED"],
+    ],
+  },
+}
+
+export default dict
