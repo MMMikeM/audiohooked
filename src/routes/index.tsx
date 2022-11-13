f7ae1846b9d3178f16f2789ab00c7cc35c3d4402
MMMike <mikem.tba@gmail.com>
2022-11-13 23:03:19 +0200

    feat: Update homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,48 +1,24 @@
-import { useI18n } from "@solid-primitives/i18n"
-import { For } from "solid-js"
-import Data from "~/dict"
-import "./index.css"
+import { createSignal } from "solid-js"
 
 export default function Home() {
-  const [t, { locale }] = useI18n()
+  const [disabled, setDisabled] = createSignal(false)
 
   return (
-    <main bg="dark-900" min-h="screen">
-      <column container p="t-12 x-4" m="x-auto" bg="dark-800">
-        <h1 text="light-900 2xl" font="semibold">
-          {t("title")}
+    <main min-h="screen">
+      <column container p="t-12 x-4" m="x-auto">
+        <h1 text="dark-900 2xl" font="semibold">
+          Welcome back
         </h1>
-        <row gap="2" p="2">
-          <column class="w-1/2" text="light-900" gap="1">
-            <h2>{t("features")}</h2>
-            <ul list="zero-decimal" text="lg" pl="8" gap="2" flex="~ col">
-              <For each={Data.en.points}>{(p, i) => <Point point={p} index={i()} />}</For>
-            </ul>
-          </column>
+        <row gap="12">
+          <button btn="secondary" disabled={disabled()}>
+            Hi
+          </button>
+          <button btn="tertiary">Hi</button>
+          <button btn="primary-500" onClick={() => setDisabled(!disabled())}>
+            {disabled() ? "Enable" : "Disable"}
+          </button>
         </row>
-        <button onClick={() => locale(locale() === "en" ? "test" : "en")}>Test translation!</button>
       </column>
     </main>
-  )
-}
-
-const Point = (props: { point: string[]; index: number }) => {
-  const [t] = useI18n()
-
-  if (props.point.length === 1) {
-    return (
-      <li>
-        <h3>{t(`points.${props.index}.0`)}</h3>
-      </li>
-    )
-  }
-
-  return (
-    <li>
-      <h3>{t(`points.${props.index}.0`)}</h3>
-      {props.point.slice(1).map((_, i) => (
-        <h4>{t(`points.${props.index}.${i + 1}`)}</h4>
-      ))}
-    </li>
   )
 }
