f23854f5f983de3e5aced1aa80dc518828ac9ac9
MMMike <mikem.tba@gmail.com>
2022-11-13 23:02:35 +0200

    feat: :label: More type magic

--- shims.d.ts
+++ shims.d.ts
@@ -1,12 +1,20 @@
 import type { AttributifyAttributes } from "@unocss/preset-attributify"
 
+type BtnGrays = ["white" | "light" | "dark" | "black"]
+type BtnColors = ["primary", "secondary", "tertiary", "red"]
+type ColorRange = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+export type Btns =
+  | `${BtnColors[number]}-${ColorRange[number]}`
+  | BtnGrays[number]
+  | BtnColors[number]
+
 declare module "solid-js" {
   namespace JSX {
     interface CustomHTMLAttributes {
       center?: boolean
     }
     interface CustomButtomAttrbutes {
-      btn?: string
+      btn?: Btns
     }
     interface HTMLAttributes<T> extends AttributifyAttributes, CustomHTMLAttributes {}
     interface ButtonHTMLAttributes<T> extends CustomButtomAttrbutes {}
